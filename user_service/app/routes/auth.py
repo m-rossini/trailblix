da54486e900c9031d6523da63929a8a4e16cc61d
Marcos Tengelmann <marcos.tengelmann@gmail.com>
2024-12-29 22:54:06 +0000

    Now we can register new users

--- user_service/app/routes/auth.py
+++ user_service/app/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, current_app
-from werkzeug.security import generate_password_hash, check_password_hash
-from ..service.service_auth import login as s_login   
+from ..service.service_auth import login as s_login 
+from ..service.service_auth import register as s_register     
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -11,20 +11,15 @@ def login():
     password = data.get('password')
     
     msg, code = s_login(username, password)
-
     return jsonify(msg), code
 
 @auth_bp.route('/api/signup', methods=['POST'])
 def register():
     data = request.json
-    username = data.get('username')
+    username = data.get('email')
     password = data.get('password')
+    display_name = data.get('displayName')
+    birth_date = data.get('birthDate')
     
-    db = current_app.config['db']
-    if db.users.find_one({'username': username}):
-        return jsonify({'message': 'User already exists'}), 400
-    
-    password_hash = generate_password_hash(password)
-    db.users.insert_one({'username': username, 'password_hash': password_hash})
-    
-    return jsonify({'message': 'User registered successfully'}), 201
+    msg, code = s_register(username, password, display_name, birth_date)
+    return jsonify(msg), code
--- user_service/app/service/service_auth.py
+++ user_service/app/service/service_auth.py
@@ -1,10 +1,39 @@
+import logging
 from flask import current_app
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+
+# Configure logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def login(username, password):
-    db = current_app.config['db']
-    user_data = db.users.find_one({'username': username})
+    logger.info("Logging in user")
+    db = current_app.config["db"]
+    user_data = db.users.find_one({"username": username})
+
+    if user_data and check_password_hash(user_data["password_hash"], password):
+        logger.info(f"User {username} logged in successfully.")
+        return "Login successful", 200
+    logger.warning(f"Failed login attempt for username: {username}")
+    return "Unauthorized", 401
+
+def register(username, password, display_name, birth_date):
+    logger.info(f"Registering user: {username}")
+    db = current_app.config["db"]
+    user_data = db.users.find_one({"email": username})
     
-    if user_data and check_password_hash(user_data['password_hash'], password):
-        return 'Login successful', 200
-    return 'Unauthorized', 401
+    if user_data:
+        logger.warning(f"Registration failed: User {username} already exists. User data: {user_data}")
+        return "User already exists", 400
+
+    password_hash = generate_password_hash(password)
+    db.users.insert_one({
+        "email": username,
+        "password_hash": password_hash,
+        "display_name": display_name,
+        "birth_date": birth_date,
+        "insert_date": datetime.utcnow()
+    })
+    logger.info(f"User {username} registered successfully.")
+    return "User registered successfully", 201
